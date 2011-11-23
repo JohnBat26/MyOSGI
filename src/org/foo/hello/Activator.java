a764e938562902b7f9022a365be1282d37a017ca
Eugene Batogov <johnbat26@gmail.com>
2011-11-23 23:17:56 +0400

    initial commit

---
+++ src/org/foo/hello/Activator.java
@@ -0,0 +1,16 @@
+package org.foo.hello;
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    public void start(BundleContext ctx) {
+        Greeting.instance = new Greeting("lifecycle");
+    }
+
+    public void stop(BundleContext ctx) {
+        Greeting.instance = null;
+    }
+}
+
